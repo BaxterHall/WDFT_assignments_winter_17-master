7b5300a009a7bfeae68c116910cb7a91fecbab92
Baxter Hall <abaxterhall@gmail.com>
2017-01-26 16:15:17 -0500

    update

--- chapter_1/jquery_assignment/dom_manipulation_challenge/dom_manipulation_challenge/js/app.js
+++ chapter_1/jquery_assignment/dom_manipulation_challenge/dom_manipulation_challenge/js/app.js
@@ -5,30 +5,32 @@ Great job on this assignment.  Everything looks good, just try to remember to be
 //Write your jQuery code in this file
 $("#edit-header").html("The jQuery Challenge");
 
-$("#box1").css({'background-color': "green" });
+$("#box1").css({ 'background-color': "green" });
 
-$("#para1").css({'font-size':"38px", "font-weight":"bold"});
+$("#para1").css({ 'font-size': "38px", "font-weight": "bold" });
 
 $(".box2").addClass('spin');
 
-$("#button1").click(function(){
+$("#button1").click(function () {
     alert('Have You Listened to A Sting Song Today?');
 })
 
-$("#box3").hover(function(){
-    $("#box3").css({'background-color': "green"})
-},  function() {
-     $("#box3").css({'background-color': "red"
-});
+$("#box3").hover(function () {
+    $("#box3").css({ 'background-color': "green" })
+}, function () {
+    $("#box3").css({
+        'background-color': "red"
+    });
 });
 
-$("#input1").keyup(function() {
+$("#input1").keyup(function () {
     console.log(
         $(this).val()
-)
+    )
 });
 
 $("#box4").animate({
-    left:"399px",
-    top:"149px"}, 2000)
-           ;
+    left: "399px",
+    top: "149px"
+}, 2000)
+    ;
