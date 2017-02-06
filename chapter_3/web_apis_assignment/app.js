4fc1d6ca6ccdc154b190eb28fd1921b0c48db2be
frshock <will.s.crane@gmail.com>
2017-02-06 12:51:53 -0500

    Review

--- chapter_3/web_apis_assignment/app.js
+++ chapter_3/web_apis_assignment/app.js
@@ -1,6 +1,12 @@
+/*Web API Review
+Nice job on this assignment.  You've parsed data.body three times when really you only needed to do it once.  From there you can access each value in the object
+the same way you've done, but all from the same variable instead of tZ, temp, and sum.  Also you'll notice that when you console log the data you get back, it's 
+returning data in the imperial scale instead of metric, there may have been something in the DarkSky API documentation to change that.
+*/
+
 //instal npm and define their requirements
 
-const https = require('https');
+const https = require('https'); //Try not to require modules that you aren't using in your code.
 const cheerio = require('cheerio');
 const request = require('request');
 
@@ -11,7 +17,7 @@ request(url, function(error,data) {
     let $ = cheerio.load(data);
     let tZ = JSON.parse(data.body);
     let temp = JSON.parse(data.body);
-    let sum = JSON.parse(data.body)
+    let sum = JSON.parse(data.body)   // Remember to include semicolons after your declarations!
     //define data in a string 
     //specifiy 'directory' of desired data in string
     // console log variable with directory location
@@ -22,7 +28,7 @@ request(url, function(error,data) {
      
 
         
-         if (error)
+         if (error) // Remember to define the scope of if statements like this using {} curly brackets.
             console.log("We’ve encountered an error: " + error);
 });
 
--- chapter_3/web_scraping_assignment/app.js
+++ chapter_3/web_scraping_assignment/app.js
@@ -1,8 +1,15 @@
+/*Web Scraping Review
+Good job! Looks like you've got a good idea as to how you would go about scraping a page for specific data.  You'll notice that you've repeated yourself
+quite a bit by writing out three requests.  You could have written a loop that would have sent all three requests with less code involved.  Something else
+you may want to look into is the idea of recursion, which is essentially writing a function that will call itself upon completion.  Concepts like that will
+help drastically reduce the amount of code you have to write.
+*/
+
 const request = require('request');
 const cheerio = require('cheerio');
 const url0 = "http://www.reddit.com";
-const url1 = "http://www.reddit.com/?count=25&after=t3_5r7ps5"
-const url2 = "http://www.reddit.com/?count=50&after=t3_5r80l5"
+const url1 = "https://www.reddit.com/?count=25&after=t3_5sapnc"  //Remember semicolons after all your declarations.
+const url2 = "https://www.reddit.com/?count=50&after=t3_5sbuk3"
 
 request(url0, function (error, response, body) {
     let str = ""
@@ -14,7 +21,7 @@ request(url0, function (error, response, body) {
     str += text
 
    
-        if (error)
+        if (error) //Remember to include curly brackets when you write if statements like this.
             console.log("We’ve encountered an error: " + error);
     }
 
