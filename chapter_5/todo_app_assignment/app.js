01454eef5796c9c111cc3035e34e89fe2faa44f6
Baxter Hall <abaxterhall@gmail.com>
2017-02-17 16:23:32 -0500

    To-Do complete

--- chapter_5/todo_app_assignment/app.js
+++ chapter_5/todo_app_assignment/app.js
@@ -13,6 +13,8 @@ class App extends React.Component {
 		this.clickHandleV2 = this.clickHandleV2.bind(this);
 		this.addTodo = this.addTodo.bind(this);
 		this.statusChange = this.statusChange.bind(this);
+		this.clearComplete = this.clearComplete.bind(this);
+
 
 
 
@@ -32,7 +34,7 @@ class App extends React.Component {
 
 	addTodo(val) {
 		event.preventDefault();
-		console.log(val)
+		// console.log(val)
 
 		const todo = { text: val, done: false, id: this.state.todos.length + 1 }
 
@@ -40,8 +42,13 @@ class App extends React.Component {
 
 		newState.push(todo);
 
+		// console.log(event)
 
 		this.setState({ todos: newState });
+		event.target.addTodo.value = "";
+
+
+
 	}
 
 	statusChange(event) {
@@ -51,6 +58,18 @@ class App extends React.Component {
 		this.setState({ status: event.target.value });
 	}
 
+	clearComplete(event) {
+
+		let notCompleted = this.state.todos.filter(function (todos) {
+			if (!todos.done) return todos;
+		});
+
+		this.setState({
+			todos: notCompleted
+		});
+
+
+	};
 	render() {
 
 
@@ -58,9 +77,9 @@ class App extends React.Component {
 		const todos = this.state.todos;
 		let arrayOfThings = [];
 		if (this.state.status === "all") {
-			
+
 			arrayOfThings = todos.map((element) => {
-			return	<Todo
+				return <Todo
 					text={element.text}
 					done={element.done}
 					clickHandleV2={this.clickHandleV2}
@@ -69,7 +88,7 @@ class App extends React.Component {
 
 
 			})
-	}
+		}
 		else if (this.state.status === "complete") {
 			arrayOfThings = [];
 			for (let i = 0; i < this.state.todos.length; i++) {
@@ -119,7 +138,7 @@ class App extends React.Component {
 					<option value="complete">complete</option>
 				</select>
 
-				<button className="pull-right btn btn-default" >Clear Complete</button>
+				<button className="pull-right btn btn-default" onClick={this.clearComplete}>Clear Complete</button>
 
 
 			</div>
@@ -169,7 +188,7 @@ class Add extends React.Component {
 					<span className="input-group-btn">
 						<button className="btn btn-primary" type="submit" >Add</button>
 					</span>
-					<input className="form-control" type="text" placeholder="add a todo" ref="addTodo" />
+					<input className="form-control" type="text" placeholder="add a todo" ref="addTodo" name="addTodo" />
 				</div>
 			</form>
 
