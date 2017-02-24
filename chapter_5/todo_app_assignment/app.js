f6158fec703a694f99c9bdf57b92c4dc729c7b2f
Baxter Hall <abaxterhall@gmail.com>
2017-02-23 20:22:27 -0500

    To-do Assignment

--- chapter_5/todo_app_assignment/app.js
+++ chapter_5/todo_app_assignment/app.js
@@ -14,73 +14,44 @@ class App extends React.Component {
 		this.addTodo = this.addTodo.bind(this);
 		this.statusChange = this.statusChange.bind(this);
 		this.clearComplete = this.clearComplete.bind(this);
-
-
-
-
 	}
 	clickHandleV2(id) {
 		const newTodos = this.state.todos.map((todos) => {
-
 			if (todos.id == id) {
 				return { text: todos.text, done: !todos.done, id: todos.id };
 			} else {
 				return todos;
 			}
-
 		})
 		this.setState({ todos: newTodos });
-		
 	}
-
-	
-
 	addTodo(val) {
 		event.preventDefault();
 		// console.log(val)
 
 		const todo = { text: val, done: false, id: this.state.todos.length + 1 };
-
 		const newState = this.state.todos;
-
 		newState.push(todo);
-
 		// console.log(event)
-
 		this.setState({ todos: newState });
 		event.target.addTodo.value = "";
-
-
-
 	}
-
 	statusChange(event) {
-
 		event.target.value;
-
 		this.setState({ status: event.target.value });
 	}
-
 	clearComplete(event) {
-
 		let notCompleted = this.state.todos.filter(function (todos) {
 			if (!todos.done) return todos;
 		});
-
 		this.setState({
 			todos: notCompleted;
 		});
-
-
 	};
 	render() {
-
-
-
 		const todos = this.state.todos;
 		let arrayOfThings = [];
 		if (this.state.status === "all") {
-
 			arrayOfThings = todos.map((element) => {
 				return <Todo
 					text={element.text}
@@ -88,8 +59,6 @@ class App extends React.Component {
 					clickHandleV2={this.clickHandleV2}
 					id={element.id}
 				/>
-
-
 			})
 		}
 		else if (this.state.status === "complete") {
@@ -104,7 +73,6 @@ class App extends React.Component {
 				}
 			}
 		}
-
 		else if (this.state.status === "active") {
 			arrayOfThings = [];
 			for (let i = 0; i < this.state.todos.length; i++) {
@@ -114,77 +82,39 @@ class App extends React.Component {
 						done={this.state.todos[i].done}
 						clickHandleV2={this.clickHandleV2}
 						id={this.state.todos[i].id} />)
-
 				}
 			}
 		};
-
-
 		return (
 			<div className="container">
 				<h1 className="text-center">todos</h1>
-
 				<Add addTodo={this.addTodo} />
-
 				<ul className="list-group">
 					{arrayOfThings}
-
 				</ul>
-
-
-
-
-
 				<select value={this.state.status} onChange={this.statusChange}>
 					<option value="all">all</option>
 					<option value="active">active</option>
 					<option value="complete">complete</option>
 				</select>
-
 				<button className="pull-right btn btn-default" onClick={this.clearComplete}>Clear Complete</button>
-
-
 			</div>
-
 		)
 	}
 };
-
-
 class Todo extends React.Component {
-
-
-
 	render() {
-
-
-
 		return (
-
 			<li className="list-group-item">
 				<input id={this.props.id} type="checkbox" checked={this.props.done} value={this.props.done ? "completed" : "active"} onChange={() => { this.props.clickHandleV2(this.props.id) }} />
 				<label className={this.props.done ? "done" : ""}>{this.props.text}</label>
 			</li>
-
 		)
 	}
-
-
 }
-
 class Add extends React.Component {
-
-
-
-
-
 	render() {
-
-
-		;
-
 		return (
-
 			<form onSubmit={() => { this.props.addTodo(this.refs.addTodo.value) }}>
 				<div className="input-group">
 					<span className="input-group-btn">
@@ -193,14 +123,7 @@ class Add extends React.Component {
 					<input className="form-control" type="text" placeholder="add a todo" ref="addTodo" name="addTodo" />
 				</div>
 			</form>
-
-
 		)
-
-
 	}
-
 };
-
-
 ReactDOM.render(<App />, document.getElementById('app'));
