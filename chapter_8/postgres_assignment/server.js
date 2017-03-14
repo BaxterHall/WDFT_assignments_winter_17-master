f4ac482e725f0c8de5f79ca6fc77074874162c6c
frshock <will.s.crane@gmail.com>
2017-03-14 14:23:57 -0400

    Review

--- chapter_8/postgres_assignment/server.js
+++ chapter_8/postgres_assignment/server.js
@@ -1,3 +1,7 @@
+/* Postgres Review
+Good job overall, I left some more specific review further down. You may want to clean up your endpoints and take a look at your error handling as right now it's 
+not working how you think it is.
+*/
 const express = require('express');
 const app = express()
 
@@ -116,8 +120,8 @@ app.get('/cars', (req, res) => {
         .then(Car => {
             res.json(Car)
         })
-        .catch(err)
-    return
+        .catch(err)                             //You're going to get an error every time you hit an endpoint with this .catch, you have to treat it the same as your 
+    return                                      //.then, in that it's a function.  Your return + console logs should be within the scope of the catch.
     console.log(err)
     console.log('no cars found matching')
 })
@@ -134,11 +138,11 @@ app.get('/cars/:id', (req, res) => {
     console.log('no cars found matching criteria')
 });
 // POST: Save a single car object
-app.post('/cars/:make&:model&:year&:dealership_id', (req, res) => {
-    new Car({
-        make: req.params.make,
-        model: req.params.model,
-        year: req.params.year,
+app.post('/cars/:make&:model&:year&:dealership_id', (req, res) => {  //This is a case where using the body of your request would be better, as right now 
+    new Car({                                                        //your endpoint is quite bloated with all the req parameters. Try changing this on 
+        make: req.params.make,                                       //your other endpoints, it will really clean up how your endpoints look and how you 
+        model: req.params.model,                                     //format your new Cars. It also makes your endpoints much more readable, right now it's 
+        year: req.params.year,                                       //kind of hard to distinguish which endpoint does what.
         dealership_id: req.params.dealership_id,
     })
         .save()
@@ -229,8 +233,8 @@ app.post('/dealership/:make&:city&:province&:postal_code&:street', (req, res) =>
 })
 // PUT: Change attributes for a single dealership object
 app.put('/dealership/:id&:make&:city&:province&:postal_code&:street', (req, res) => {
-    let newDealer = new Dealer({
-        make: req.params.make,
+    let newDealer = new Dealer({        //This is another example of where req.params is just too long, this endpoint is a pain in the ass to read.
+        make: req.params.make,          //Try using queries or req.body to really clean this up.
         city: req.params.city,
         province: req.params.province,
         postal_code: req.params.postal_code,
